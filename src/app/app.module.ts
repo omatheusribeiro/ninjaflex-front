9e2e3458f5ae2bce162d20b46cb92cdef2b0e4d0
Matheus Ribeiro <omatheusribeiro@outlook.com>
2022-03-31 18:36:28 -0300

    Created new sidenav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { HomeModule } from './modules/home/home.module';
+import { SideNavNinjaFlexComponent } from './modules/common/components/sidenav-ninjaflex/sidenav-ninjaflex.component';
 // import { RequestInterceptor } from '../app/modules/common/interceptors/request/request-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavNinjaFlexComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
     HomeModule
   ],
   providers: [],
